3edf0bcaaa24406827ad8410ce101b981b5c6910
ob-star <bright.okyere@ashesi.edu.gh>
2026-02-09 15:27:53 +0000

    portfolio update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ const Header: React.FC = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center space-x-2">
             <Code className="h-8 w-8 text-blue-500" />
-            <span className="text-xl font-bold text-white">Bright Okyere</span>
+            <a  href='/' className="text-xl font-bold text-white"><User/> </a>
           </div>
 
           {/* Desktop Navigation */}
